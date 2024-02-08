c9896e33b9ccadb5ebafa1bd6db7f5f885d1e558
Emiliano Bruni <info@ebruni.it>
2024-02-08 11:01:26 +0100

    Add pm2 config

---
+++ %%app%%-%%service%%/src/configs/pm2.config.cjs
@@ -0,0 +1,20 @@
+// env_development e env_production non funzionano con le instances per cui
+const app_env_cfg = process.env.NODE_ENV == 'development' 
+    ? { exec_mode: "fork", instances: 1, watch: true}
+    : {};
+
+module.exports = {
+    apps: [
+        {
+            name: "apimac-www",
+            script: "server.js",
+            autorestart: true,
+            max_memory_restart: "1G",
+            exec_mode: "cluster",
+            instances: 4,
+            watch: false,
+            ...app_env_cfg
+        }
+    ],
+};
+
